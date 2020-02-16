d10b89075db2d97329e9eeec81c38885ade7c9ca
josedani08200 <jose.dani08200@gmail.com>
2020-02-16 16:24:10 -0500

    create result component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Result from './Result'
 
 export default function Home() {
   /*****************************************************
@@ -39,27 +40,41 @@ export default function Home() {
 
   return (
     <>
-      <div id="background-img">
-      <div className="search-wrapper">
-        <div className="search-box">
-            <h1>Munchies</h1>
-            <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="Restaurants"
-                onChange={handleChange}
-            />
-            </form>
-        </div>
-      </div>
-        <div>
-          <ul>
-            {apiData.map(item => {
-              return <p>{item.name}</p>;
-            })}
-          </ul>
-        </div>
-      </div>
+    
+      {
+          (apiData.length === 0) ? (
+            <div id="background-img">
+          <div className="search-wrapper">
+              <div className="search-box">
+                  <h1>Munchies</h1>
+                  <form onSubmit={handleSubmit}>
+                      <input
+                          type="text"
+                          placeholder="Restaurants"
+                          onChange={handleChange}
+                      />
+                  </form>
+              </div>
+          </div>
+          </div>) : ( 
+            <div>
+                <ul>
+                    {apiData.map(item => {
+                    return (
+                        <Result 
+                            name={item.name}
+                            image={item.image_url}
+                            location={`${item.location.address1} ${item.location.city} ${item.location.zip_code}, ${item.location.state}`}
+                            rating={`${item.rating}, star review.`}
+                        />
+                    );
+                    })}
+                </ul>
+            </div>
+        )
+      }
+           
+    
     </>
   );
 }
